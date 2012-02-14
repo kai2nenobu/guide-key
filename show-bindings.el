27933a0ea12ead2b2bed5e3ebb15fe361cb339d7
Tsunenobu Kai <kbkbkbkb1@gmail.com>
2012-02-15 05:02:25 +0900

    Implement `show-bindings:format-guide-buffer'. This function forms guide buffer.
    This function makes guide buffer from `describe-buffer-bindings'
    buffer. Unnecessary strings are deleted.

--- show-bindings.el
+++ show-bindings.el
@@ -1,7 +1,7 @@
 
 (require 'popwin)
 
-(defvar show-bindings:show-key-sequence '("\C-c" "\C-q" "\C-xr")
+(defvar show-bindings:show-key-sequence '("\C-c" "\C-c&" "\C-q" "\C-xr" "\C-x4")
   "*Key sequences to show its bindings.")
 
 (defvar show-bindings:polling-time 0.05
@@ -41,12 +41,13 @@
           (with-current-buffer (get-buffer-create show-bindings:buffer-name)
             (erase-buffer)
             (describe-buffer-bindings cbuffer key-seq)
+            (show-bindings:format-guide-buffer key-seq)
             (popwin:popup-buffer (current-buffer) :width 65 :position 'right :noselect t))
             ;; (popwin:popup-buffer (current-buffer) :height 20 :position 'bottom :noselect t))
           )
       (when (show-bindings:poppedup-p)
         ;; (delete-windows-on show-bindings:buffer-name))
-        (popwin:close-popup-window))
+        (popwin:close-popup-window t))
       )
     (setq show-bindings:last-command-keys-vector key-seq)))
 
@@ -73,18 +74,36 @@
   "Turn off polling timer."
   (cancel-timer show-bindings:polling-timer))
 
+(defun show-bindings:format-guide-buffer (key-seq)
+  "Format a guide buffer."
+  (let ((key-dsc (key-description key-seq))
+        (last-end-pt 1))
+    (untabify (point-min) (point-max))    ; replace tab to space
+    (beginning-of-buffer)
+    (while (re-search-forward
+            (format "^%s \\([^ \t]+\\)\\([ \t]+\\)\\(\\(?:[^ \t\n]+ ?\\)+\\)$" key-dsc) nil t)
+      (replace-match "[\\1]\\2\\3")
+      (when (> (- (point-at-bol) last-end-pt) 0)
+        (delete-region last-end-pt (point-at-bol)))
+      (setq last-end-pt (point-at-bol 2)))
+    (delete-region last-end-pt (progn (end-of-buffer) (point)))
+    (beginning-of-buffer)
+    ))
+
 ;;; debug
 (defun show-bindings:message-events ()
   ""
   (interactive)
   (message (format "lce:%S tck:%S tckv:%S lie:%S uce:%S popb:%S cls:%S"
-           last-command-event
-           (this-command-keys)
-           (this-command-keys-vector)
-           last-input-event
-           unread-command-events
-           popwin:popup-buffer
-           (show-bindings:poppedup-p)
-           )))
+                   last-command-event
+                   (this-command-keys)
+                   (this-command-keys-vector)
+                   last-input-event
+                   unread-command-events
+                   popwin:popup-buffer
+                   (show-bindings:poppedup-p)
+                   )))
 ;; (setq ttt (run-at-time t 1 'show-bindings:message-events))
 ;; (cancel-timer ttt)
+
+(show-bindings-mode t)
