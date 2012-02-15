33e8572b70ade933fc3e4997823bcbf184ac4dc6
Tsunenobu Kai <kbkbkbkb1@gmail.com>
2012-02-15 19:52:15 +0900

    Implement features to display multiple items per line, and to highlight text according to `guide-key:highlight-command-regexp'.

--- guide-key.el
+++ guide-key.el
@@ -1,17 +1,32 @@
 
 ;;; TODO:
+;; - write document
 ;; - enrichment of guide buffer
-;; - automatically guide all following keys
+;;   - select color to be used
+;; - DONE automatically guide all following keys
 ;; - pop up guide buffer at top or bottom
 ;; - cooperate with polling of popwin.el?
+;; - confine a length of command name
+;; - confine the number of items to guide
+;; - a feature to exclude or include guide by command name
+;; - define (buffer local) minor mode instead of global minor mode
+
+(eval-when-compile
+  (require 'cl))
 
 (require 'popwin)
 
-(defvar guide-key:show-key-sequence '("C-c" "C-c &" "C-q" "C-x r" "C-S-r" "C-x 4" "C-4")
+(defvar guide-key:show-key-sequence '("C-x" "C-x 8" "C-x 8 ^" "C-c" "C-c &" "C-q" "C-x r" "C-S-r" "C-x 4" "C-4")
   "*Key sequences to show its bindings.")
 
-(defvar guide-key:polling-time 0.01
+(defvar guide-key:polling-time 0.1
   "*Polling time to show bindings.")
+
+(defvar guide-key:highlight-command-regexp "forward\\|rectangle"
+  "*Regexp of command to highlight.")
+
+(defvar guide-key:guide-list nil
+  "List of key guide. Element of this is like (KEY SPACE COMMAND).")
 
 (defface guide-key:key-face
   '((t (:foreground "red")))
@@ -21,6 +36,11 @@
 (defface guide-key:prefix-command-face
   '((t (:foreground "cyan")))
   "Face for prefix command"
+  :group 'guide-key)
+
+(defface guide-key:highlight-command-face
+  '((t (:foreground "yellow")))
+  "Face for command to highlight"
   :group 'guide-key)
 
 (defvar guide-key:polling-timer nil
@@ -60,9 +80,12 @@
             (describe-buffer-bindings cbuffer key-seq)
             (guide-key:format-guide-buffer key-seq)
             (if (> (setq max-width (guide-key:buffer-max-width)) 0)
-                (popwin:popup-buffer (current-buffer)
-                                     :width (+ max-width 3) :position 'right :noselect t)
-              (message "No following key.")))))
+                (progn
+                  (guide-key:pre-command-popup-close)
+                  (popwin:popup-buffer (current-buffer)
+                                     :width (+ max-width 3) :position 'right :noselect t))
+              (message "No following key."))))
+      (guide-key:pre-command-popup-close))
     (setq guide-key:last-command-keys-vector key-seq)))
 
 (defun guide-key:pre-command-popup-close ()
@@ -102,27 +125,40 @@
 
 (defun guide-key:format-guide-buffer (key-seq)
   "Format a guide buffer."
-  (let ((key-dsc (key-description key-seq))
+  (let ((guide-key:guide-list nil)
+        (key-dsc (key-description key-seq))
         (last-end-pt 1))
     (untabify (point-min) (point-max))    ; replace tab to space
     (goto-char (point-min))
-    ;; delete extra strings and format key guide
+    ;; extract key guide from buffer bindings
     (while (re-search-forward
             (format "^%s \\([^ \t]+\\)\\([ \t]+\\)\\(\\(?:[^ \t\n]+ ?\\)+\\)$" key-dsc) nil t)
-      (replace-match "[\\1]\\2\\3")
-      (when (> (- (point-at-bol) last-end-pt) 0)
-        (delete-region last-end-pt (point-at-bol)))
-      (setq last-end-pt (point-at-bol 2)))
-    (delete-region last-end-pt (point-max))
+      (add-to-list 'guide-key:guide-list
+                   (list (match-string 1) (match-string 2) (match-string 3)) t))
     ;; fontify key guide
-    (goto-char (point-min))
-    (while (re-search-forward
-            "\\(\\[[^ \t]+\\]\\)\\([ \t]+\\)\\(\\(?:[^ \t\n]+ ?\\)+\\)$" nil t)
-      (put-text-property (match-beginning 1) (1+ (match-beginning 1)) 'face 'guide-key:key-face)
-      (put-text-property (1- (match-end 1)) (match-end 1) 'face 'guide-key:key-face)
-      )
+    (erase-buffer)
+    (loop with item-per-line = (1+ (/ (length guide-key:guide-list) (window-height)))
+          for (key space command) in guide-key:guide-list
+          for column from 1
+          do (insert (guide-key:fontified-string key space command)
+                     (if (= (mod column item-per-line) 0) "\n" " ")))
+    (align-regexp (point-min) (point-max) "\\(\\s-*\\) \\[" 1 1 t)
     (goto-char (point-min))
     ))
+
+(defun guide-key:fontified-string (key space command)
+  "Fontified string for key guide"
+  (format "%s %s"
+          (concat (propertize "[" 'face 'guide-key:key-face)
+                  key
+                  (propertize "]" 'face 'guide-key:key-face))
+          ;; space
+          (cond ((string-match "prefix" command)
+                 (propertize command 'face 'guide-key:prefix-command-face))
+                ((string-match guide-key:highlight-command-regexp command)
+                 (propertize command 'face 'guide-key:highlight-command-face))
+                (t
+                 command))))
 
 (defun guide-key:buffer-max-width ()
   "Return max width in current buffer."
@@ -132,7 +168,6 @@
 ;;; debug
 (defun guide-key:message-events ()
   ""
-  (interactive)
   (message (format "lce:%S tck:%S tckv:%S lie:%S uce:%S popb:%S cls:%S"
                    last-command-event
                    (this-command-keys)
