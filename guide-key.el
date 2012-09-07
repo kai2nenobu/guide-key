9861e50170799f235ac41b8596e29c9f4a382ffc
Tsunenobu Kai <kbkbkbkb1@gmail.com>
2012-09-08 06:55:28 +0900

    Discard a check of minor mode.
    `guide-key-mode' is always enabled during execution of polling-function.

--- guide-key.el
+++ guide-key.el
@@ -150,25 +150,24 @@ positive, and disable it otherwise."
 ;;; internal functions
 (defun guide-key:polling-function ()
   "Polling function executed every `guide-key:polling-time' second."
-  (when guide-key-mode
-    (let ((dsc-buf (current-buffer))
-          (hi-regexp guide-key:highlight-command-regexp)
-          (key-seq (this-command-keys-vector))
-          (max-width 0))
-      (if (guide-key:popup-guide-buffer-p key-seq)
-          (when (guide-key:update-guide-buffer-p key-seq)
-            (with-current-buffer (get-buffer-create guide-key:guide-buffer-name)
-              (unless truncate-lines (setq truncate-lines t))   ; don't fold line
-              (when indent-tabs-mode (setq indent-tabs-mode nil)) ; don't use tab as white space
-              (erase-buffer)
-              (describe-buffer-bindings dsc-buf key-seq)
-              (if (> (guide-key:format-guide-buffer key-seq hi-regexp) 0)
-                  (progn
-                    (guide-key:close-guide-buffer)
-                    (guide-key:popup-guide-buffer))
-                (message "No following key."))))
-        (guide-key:close-guide-buffer))
-      (setq guide-key:last-key-sequence-vector key-seq))))
+  (let ((dsc-buf (current-buffer))
+        (hi-regexp guide-key:highlight-command-regexp)
+        (key-seq (this-command-keys-vector))
+        (max-width 0))
+    (if (guide-key:popup-guide-buffer-p key-seq)
+        (when (guide-key:update-guide-buffer-p key-seq)
+          (with-current-buffer (get-buffer-create guide-key:guide-buffer-name)
+            (unless truncate-lines (setq truncate-lines t))   ; don't fold line
+            (when indent-tabs-mode (setq indent-tabs-mode nil)) ; don't use tab as white space
+            (erase-buffer)
+            (describe-buffer-bindings dsc-buf key-seq)
+            (if (> (guide-key:format-guide-buffer key-seq hi-regexp) 0)
+                (progn
+                  (guide-key:close-guide-buffer)
+                  (guide-key:popup-guide-buffer))
+              (message "No following key."))))
+      (guide-key:close-guide-buffer))
+    (setq guide-key:last-key-sequence-vector key-seq)))
 
 (defun guide-key:popup-guide-buffer ()
   "Pop up a guide buffer."
