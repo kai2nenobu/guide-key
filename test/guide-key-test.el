b6cc4de0fc1c6591dd99e145ddcdcb76638d61be
Tsunenobu Kai <kbkbkbkb1@gmail.com>
2014-08-06 02:55:49 +0900

    [test] Define a test macro surrounded by setup and teardown functions

--- test/guide-key-test.el
+++ test/guide-key-test.el
@@ -3,6 +3,29 @@
 (eval-when-compile
   (require 'cl))
 
+(defconst guide-key-test/global-keybindings
+  '((1 . guide-key-test/global-keybinding1)
+    (2 . guide-key-test/global-keybinding2)))
+
+(defconst guide-key-test/prefix-key (kbd "s-S-C-M-x"))
+
+(defmacro guide-key-test/deftest (name doc-string &rest body)
+  (declare (indent 1)
+           (doc-string 2))
+  `(ert-deftest ,(intern (concat "guide-key-test/" (symbol-name name))) ()
+     ,doc-string
+     ;; setup
+     (loop for (event . definition) in guide-key-test/global-keybindings
+           do
+           (global-set-key (vconcat guide-key-test/prefix-key (vector event)) definition))
+     ;; test
+     ,@body
+     ;; teardown
+     (loop for (event . definition) in guide-key-test/global-keybindings
+           do
+           (global-unset-key (vconcat guide-key-test/prefix-key (vector event))))
+     ))
+
 (ert-deftest guide-key-test/get-highlight-face ()
   "Test of `guide-key/get-highlight-face'"
   (let ((guide-key/highlight-command-regexp
